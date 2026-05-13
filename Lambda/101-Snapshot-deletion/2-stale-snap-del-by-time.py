ea6f25088ce900f667db6c3f47e5c6deaad5793e
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2026-05-13 23:58:11 +0530

    Create 2-stale-snap-del-by-time.py
    Lambda script to delete the stale snapshots which are older than some specific time

---
+++ Lambda/101-Snapshot-deletion/2-stale-snap-del-by-time.py
@@ -0,0 +1,33 @@
+import boto3
+from botocore.exceptions import ClientError
+from datetime import datetime, timezone, timedelta
+
+def lambda_handler(event, context):
+
+    ec2 = boto3.client('ec2')
+
+    # Snapshots older than 30 days
+    cutoff_date = datetime.now(timezone.utc) - timedelta(days=1)  # We can change the date according to our needs
+
+    # Get all snapshots owned by this account
+    snapshots = ec2.describe_snapshots(OwnerIds=['self'])
+
+    for snapshot in snapshots['Snapshots']:
+
+        snapshot_id = snapshot['SnapshotId']
+        snapshot_time = snapshot['StartTime']
+
+        print(f"Checking snapshot: {snapshot_id}")
+
+        # Delete only if snapshot is older than 30 days
+        if snapshot_time < cutoff_date:
+
+            try:
+                ec2.delete_snapshot(SnapshotId=snapshot_id)
+                print(f"Deleted snapshot {snapshot_id} - older than 1 day") 
+
+            except ClientError as e:
+                print(f"Failed to delete {snapshot_id}: {e}")
+
+        else:
+            print(f"Keeping snapshot {snapshot_id} - newer than 1 day")
